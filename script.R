5163cd3e7ca9922ba3caeefed18c28c7e0b8656c
ajstewartlang <andrew.stewart@manchester.ac.uk>
2019-06-26 13:21:15 +0100

    first commit

---
+++ script.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+
+mtcars %>%
+  mutate(cyl = factor(cyl)) %>%
+  ggplot(aes(x = cyl, y = mpg)) +
+  geom_violin(aes(fill = cyl)) +
+  stat_summary(fun.data = "mean_cl_boot") +
+  labs(x = "Number of Cylinders", 
+       y = "MPG",
+       title = "Fuel Economy (MPG) by Number of Engine Cylinders",
+       subtitle = "mtcars dataset") +
+  guides(fill = FALSE)
